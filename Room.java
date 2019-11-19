3e5e466742b3e409a011f25b8107696c84af97db
Frederik <Frederik.skarbyottosen@gmail.com>
2019-11-19 15:21:51 +0100

    Room class changes

--- Room.java
+++ Room.java
@@ -1,3 +1,39 @@
 public class Room {
+
+   //Atributes
    private int RoomID;
+   private int numofbeds;
+   private boolean internet;
+   private int floor;
+   private int price;
+   private boolean availability;
+   
+   public Room(int RoomID, int numofbeds, boolean internet, int floor, int price, boolean avaiability){
+       this.RoomID = RoomID;
+       this.numofbeds = numofbeds;
+       this.internet = internet;
+       this.phonenumber = phonenumber;
+       this.GuestID = GuestID;
+   }
+
+   public Guest() {}
+   
+   //Getters and setters
+   public int getRoomID()  {
+       return RoomID;
+   }
+   
+   public void setRoomID(int RoomID)
+   {
+       this.RoomID = RoomID;
+   }
+   public int getNumofBeds()  {
+       return numofbeds;
+   }
+   
+   public void setNumofBeds(int numofbeds)
+   {
+       this.numofbeds = numofbeds;
+   }
+
 }
