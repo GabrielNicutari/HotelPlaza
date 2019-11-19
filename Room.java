be1f9e017638b4f2431491ce3478ce1bd7873db1
Frederik <Frederik.skarbyottosen@gmail.com>
2019-11-19 15:38:31 +0100

    Room class

--- Room.java
+++ Room.java
@@ -8,15 +8,16 @@ public class Room {
    private int price;
    private boolean availability;
    
-   public Room(int RoomID, int numOfBeds, boolean internet, int floor, int price, boolean avaiability){
+   public Room(int RoomID, int numOfBeds, boolean internet, int floor, int price, boolean availability){
        this.RoomID = RoomID;
        this.numOfBeds = numOfBeds;
        this.internet = internet;
-       this.phonenumber = phonenumber;
-       this.GuestID = GuestID;
+       this.floor = floor;
+       this.price = price;
+       this.availability = availability;
    }
 
-   public Guest() {}
+   public Room() {}
    
    //Getters and setters
    public int getRoomID()  {
@@ -34,5 +35,38 @@ public class Room {
    public void setNumOfBeds(int numOfBeds)   {
        this.numOfBeds = numOfBeds;
    }
+   public boolean getInternet(){
+       return internet;
+   }
+   public void setInternet(boolean internet){
+       this.internet = internet;
+   }
+   public int getFloor()  {
+       return floor;
+   }
+   
+   public void setFloor(int floor)   {
+       this.floor = floor;
+   }
+   public int getPrice()  {
+       return price;
+   }
+   
+   public void setPrice(int price)   {
+       this.price = price;
+   }
+   public boolean getAvailability()  {
+       return availability;
+   }
+   
+   public void setAvailability(boolean availability)   {
+       this.availability = availability;
+   }
+   
+   //To string
+   public String toString()   {
+      return RoomID + " " + numOfBeds + " " + internet + " " + floor + " " + price + " " + availability;
+   }
+
 
 }
