6e70154f8877a02fc4597dd35de53caa1f9f7a79
Cristian <mccrys_00@yahoo.com>
2019-11-20 15:48:47 +0100

    Repo

--- Repository.java
+++ Repository.java
@@ -3,10 +3,13 @@ import java.io.*;
 
 public class Repository {
 
-   //Staff
+   //Arraylists
    
    ArrayList<Staff> staffList = new ArrayList<>();
+   ArrayList<Room> roomList = new ArrayList<>();
    
+   //Staff
+    
    //Create  
    
    public void createStaff() throws IOException {
@@ -37,6 +40,7 @@ public class Repository {
       
       System.out.println("Monthly Salary: ");
       staff.setSalary(validateInput());
+      
       staffList.add(staff);
             
       BufferedWriter output = new BufferedWriter(new FileWriter("Staff.txt", true));
@@ -45,15 +49,51 @@ public class Repository {
       output.write(staffList.get(staffList.size() - 1).toString());
       output.close();
       
+   }
      //Search
      
-      public void searchStaff() throws IOException {
+   public void searchStaff() throws IOException {
       Scanner console = new Scanner(System.in);
       
-           
-     
-     
-   }
+   } 
+   
+   //Rooms
+    
+   //Create
+   public void createRoom() throws IOException {
+      Scanner console = new Scanner(System.in);
+      Room room = new Room();
+      
+      System.out.println("In order to create a new room, please enter the following:");
+      
+      System.out.println("Number of beds in the room: ");
+      room.setNumOfBeds(validateInput());
+      
+      System.out.println("Does the room have internet?");
+      System.out.println("If no type false, if yes type true");
+      room.setInternet(console.nextBoolean());
+      
+      System.out.println("Floor:");
+      room.setFloor(validateInput());
+      
+      System.out.println("Price per. night");
+      room.setPrice(validateInput());
+      
+      room.setAvailability(true);
+      
+      //Adding the room object to the arraylist.
+      roomList.add(room);
+      
+      BufferedWriter output = new BufferedWriter(new FileWriter("Room.txt", true));
+      
+      output.newLine();
+      output.write(roomList.get(roomList.size() - 1).toString());
+      output.close();
+      
+      }
+      
+
+  
    
    public int validateInput()  {
       Scanner console = new Scanner(System.in);
